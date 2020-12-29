8b59e0a2c2082004f80e794040e35781bdeab55f
Krystian <krystian.izdebski1999@gmail.com>
2020-12-29 02:06:37 +0100

    Added SQL DAO for Student object

---
+++ src/main/java/pl/krystian/RestApiPractising/StudentRepository.java
@@ -0,0 +1,39 @@
+package pl.krystian.RestApiPractising;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentRepository {
+	
+	Student student;
+	List<Student> studentsList = new ArrayList<Student>();
+	
+	public List<Student> getStudents() {
+		studentsList = new ArrayList<Student>();
+		try{  
+			Class.forName("com.mysql.cj.jdbc.Driver");  
+			Connection con=DriverManager.getConnection(  
+			"jdbc:mysql://localhost:3306/Students","root","");  
+			Statement stmt=con.createStatement();  
+			ResultSet rs=stmt.executeQuery("select * from FirstClass");  
+			con.close();  
+			
+			while(rs.next()) {
+				student = new Student();
+				
+				student.setId(rs.getInt("ID"));
+				student.setFirstName(rs.getString("FirstName"));
+				student.setLastName(rs.getString("LastName"));
+				student.setAge(rs.getInt("Age"));
+				
+				studentsList.add(student);
+			}}
+		catch(Exception e){ System.out.println(e);}
+		return studentsList;
+	}
+
+}
